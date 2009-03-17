022752121fdf57c5157c165f665ba6106d3763e2
shawn <trash@pwn3d.(none)>
2009-03-17 00:04:12 -0600

    List and quit functions work, need to add ability to add new contacts and write out file

--- pyContacts.py
+++ pyContacts.py
@@ -9,51 +9,80 @@
 #
 #=Imports===
 import csv
-
+import os
 #=End Imports===
 
 #=Major Variables===
 contacts_array = []
-file=""
+#thefile="/home/trash/.contacts.csv"
+#thefile=""
 #=End Variables===
 
 
 #=Functions===
 
-#Ugly first attempt at fetch and print on an existing file
+#Retreive the contacts and place them in an array
 def fetch_contacts(filename):
-    contacts_file=csv.reader(open(filename))
-    for row in contacts_file:
+    contacts_thefile=csv.reader(open(filename))
+    for row in contacts_thefile:
         contacts_array.append( row )
 
 #add a contact to the array
 def append_contact(input):
     contacts_array.append(input)
 
-#The write and exit function
-#def kill_func():
+def prnt_contacts():
+    print "\n"
+    print "name, number"
+    i=0
+    for row in contacts_array:
+        print contacts_array[i][0] + ", " + contacts_array[i][1]
+        i=i+1
+    print "\n"
 
-#def write_to_contacts(filename)
-
+#the menu for this program shall be defined here
 def menu():
     print "Contacts Management"
     print "1. List Contacts"
     print "2. Add a contact"
     print "q. quit"
-    
+    selection=raw_input("What do you want to do?: ")
+    call_correct_function(selection)
+
+#after the menu selection has been made, determine user input and
+#call the correct function
+def call_correct_function(selection):
+    if selection == "1":
+        prnt_contacts()
+        menu()
+    elif selection == "2":
+        print "\nAdd a contact\n"
+        menu()
+    elif selection == "q":
+        quit()
+    else:
+        print "\nUnrecognized Option\n"
+        menu()
+        
+
+#write contacts to thefile after sorting
 def write_contacts(filename):
-    output_file = csv.writer(open(file, 'w'))
-    output_file.writerows(contacts_array)
+    contacts_array.sort()
+    output_thefile = csv.writer(open(thefile, 'w'))
+    output_thefile.writerows(contacts_array)
     
 #=Start the program===
-file="/home/trash/.contacts.csv"
-#append_name="someone"
-#append_number="555-555-5555"
-#input=[append_name,append_number]
-#input_contact(input)
+#thefile="/home/trash/.contacts.csv"
 #menu()
-fetch_contacts(file)
-contacts_array.sort()
-print contacts_array
-write_contacts(file)
+#fetch_contacts(thefile)
+#contacts_array.sort()
+#print contacts_array
+#write_contacts(thefile)
+thefile=raw_input("Input your contact file: ")
+if thefile=="":
+    thefile=os.path.expanduser('~') + "/.contacts.csv"
+    print "No file selected, defaulting to " + thefile 
+fetch_contacts(thefile)
+menu()
+
 #=End===
