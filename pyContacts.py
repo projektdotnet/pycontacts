b3bfaa323f19e9883a435e842f680e375faa89be
shawn <trash@pwn3d.(none)>
2009-03-21 07:19:16 -0600

    Removed comments, simplified thefile to a static assignment

--- pyContacts.py
+++ pyContacts.py
@@ -24,26 +24,25 @@
 #=Imports===
 import csv
 import os
+
 #=End Imports===
 
 #=Major Variables===
 contacts_array = []
+thefile=os.path.expanduser('~') + "/.pyContactsCSV"
 #=End Variables===
 
 #=Functions===
 
-#Retreive the contacts and place them in an array
 def fetch_contacts(filename):
     contacts_thefile=csv.reader(open(filename))
     for row in contacts_thefile:
         contacts_array.append( row )
 
-#add a contact to the array
 def append_contact(input):
     contacts_array.append(input)
     write_contacts(thefile)
 
-#Display contacts if requested
 def prnt_contacts():
     contacts_array.sort()
     print "\n"
@@ -54,7 +53,6 @@ def prnt_contacts():
         i=i+1
     print "\n"
 
-#how to remove a contact from the list
 def remove_contact():
     if len(contacts_array) > 0:
         contacts_array.sort()
@@ -72,7 +70,6 @@ def remove_contact():
     else:
         print "No Contacts\n\n"
 
-#the menu for this program shall be defined here
 def menu():
     print "Contacts Management"
     print "1. List Contacts"
@@ -82,8 +79,6 @@ def menu():
     selection=raw_input("What do you want to do?: ")
     call_correct_function(selection)
 
-#after the menu selection has been made, determine user input and
-#call the correct function
 def call_correct_function(selection):
     if selection == "1":
         prnt_contacts()
@@ -124,7 +119,6 @@ def call_correct_function(selection):
         menu()
     menu()
 
-#write contacts to thefile after sorting
 def write_contacts(filename):
     contacts_array.sort()
     output_thefile = csv.writer(open(filename, 'w'))
@@ -132,16 +126,9 @@ def write_contacts(filename):
 
 #=Start the program===
 
-#ask the user if they have a file the would like to use
-thefile=raw_input("Input your contact file: ")
-#If user doesn't specify a file assign the default or check to see if file exists
-if thefile=="":
-    thefile=os.path.expanduser('~') + "/.contacts.csv"
-    print "\nNo file selected, defaulting to " + thefile +"\n"
 if os.path.exists(thefile):
     fetch_contacts(thefile)
-else:
-    print "No contacts stored there, will create a new file on exit"
+
 menu()
 
 #=End===
