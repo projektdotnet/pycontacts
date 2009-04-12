759e7898ab596694db4ffecefb2c6cd8c0f770dd
Shawn "Prjktdtnt" Thompson <shawn@projektenterprises.com>
2009-04-12 03:55:36 -0600

    pyContacts and the CMD interface are split, now using pycmd for user interface

--- pyContacts.py
+++ pyContacts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# pyContacts v0.3.1 - Copyright Shawn "prjktdtnt" Thompson 2008
+# pyContacts v0.4.0 - Copyright Shawn "prjktdtnt" Thompson 2008
 # Simple contact manager for my own use
 #
 """
@@ -17,51 +17,40 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-# Features: *Add, remove, store contact information
-#           *Display easily readable and formatted info
-#            to the user
-#
-
-#=START Imports===
-import csv
-import os
-import operator
-
-#=END Imports===
-
-#=START Variables===
-global contacts_array
-contacts_array = [] #Used later to track all contacts
-contacts_file = os.path.expanduser('~') + "/.pyContactsCSV"
-running = True #Used for looping menu function instead of multiple re-calling
-#=END Variables===
-
-#=START Classes===
 class Contact:
     def __init__(self, id="", fname="", lname="", phone="", email="", address1="", address2="", city="", state="", zip=""):
         self.contact_id = id
-        self.contact_fname = fname
-        self.contact_lname = lname
+        #Capitalize fname and lname (for newly added contacts)
+        self.contact_fname = fname.capitalize()
+        self.contact_lname = lname.capitalize()
+        #Receive the phone info
         self.phone = phone
-        self.email = email
-        self.address1 = address1
-        self.address2 = address2
-        self.city = city
-        self.state = state
-        self.zip = zip
         self.makePhonePretty()
+        self.email = email
+        self.address1 = address1.capitalize()
+        self.address2 = address2
+        self.city = city.capitalize()
+        if len(state) == 2:
+            self.state = state.upper()
+        else:
+            self.state = state.capitalize()
+        self.zip = zip
 
     #listMe used to print a clean looking version with proper spacing
     def listMe(self):
-        print str(self.contact_id).rjust(3) + ") " + self.contact_fname.ljust(15) + "- " + self.pretty_phone.ljust(17) + "- " + self.email
+        print str(self.contact_id).rjust(3) + ") " + self.sayFullName()
 
     #List output for other functions to use
     def writeMe(self):
         return [self.contact_id,self.contact_fname,self.contact_lname,self.phone,self.email,self.address1,self.address2,self.city,self.state,self.zip]
 
-    #Return only a name, primarily used in remove function
-    def sayName(self):
+    #Return only first name
+    def sayfName(self):
         return self.contact_fname
+
+    #Return only last name 
+    def saylName(self):
+        return self.contact_lname
     
     #Return the full name of a user
     def sayFullName(self):
@@ -71,6 +60,16 @@ class Contact:
     #Return the User ID
     def sayId(self):
         return self.contact_id
+    
+    def sayPhone(self):
+        return self.phone
+
+    def sayEmail(self):
+        return self.email
+
+    def sayAddress(self):
+        return [self.address1,self.address2,self.city,self.state,self.zip]
+        
 
     #Create a user readable phone
     def makePhonePretty(self):
@@ -81,314 +80,51 @@ class Contact:
         else:
             self.pretty_phone = self.phone # If we don't know the correct output then don't attempt to format ex 815551234
 
-    #If user moves up or down in the array they'll need a new ID number
-    def reId(self, id):
-        self.contact_id = id
-
-    #Edit contact first and last name
-    def reName(self):
-        fname = str(raw_input("What is the contact's first name? "))
-        lname = str(raw_input("What is the contact's last name?  "))
-        print ""
-        print "Name: " + fname + " " + lname
-        print ""
-        name_correct = str(raw_input("Does this look correct? [y/n]"))
-        if name_correct == "y":
-            self.contact_fname = fname
-            self.contact_lname = lname
-        elif name_correct == "n":
-            self.reName()
-        else:
-            print "Invalid Response"
-
-    #Prettify the output for rePhone(self)
-    def rePhonePretty(self, phone=""):
-        if len(phone) == 7:
-            returned_number = phone[0:3] + "-" + phone[3:]
-            return returned_number
-        elif len(phone) == 10:
-            returned_number = '(' + phone[0:3] + ') ' + phone[3:6] + "-" + phone[6:]
-            return returned_number
-        else:
-            return phone
-
-    #Ability to edit phone number
-    def rePhone(self):
-        new_phone = str(raw_input("What is the contact's phone number?: "))
-        print ""
-        print "Phone: " + self.rePhonePretty(new_phone)
-        print ""
-        phone_correct = str(raw_input("Does this look correct? [y/n]"))
-        if phone_correct == "y":            
-            self.phone = new_phone
-            self.makePhonePretty()
-        elif phone_correct == "n":
-            self.rePhone()
-        else:
-            print "Invalid Response"
-
-    #Ability to add a street address
-    def reAddress(self):
-        new_address1 = str(raw_input("Address 1: "))
-        new_address2 = str(raw_input("Address 2: "))
-        new_city = str(raw_input("City: "))
-        new_state = str(raw_input("State: "))
-        new_zip = str(raw_input("Zip: "))
-        print ""
-        print "Full Address: "
-        print ""
-        print self.contact_fname + " " + self.contact_lname
-        if len(new_address2) > 0:
-            print new_address1
-            print new_address2
-        else:
-            print new_address1
-        if len(new_state) == 2:
-            new_state = new_state.upper()
-        else:
-            new_state = new_state.capitalize()
-        print new_city.capitalize() + ", " + new_state + " " + new_zip
-        print ""
-        address_correct = str(raw_input("Does this look correct? [y/n] "))
-        if address_correct == "y":
-            new_city = new_city.capitalize()
-            self.address1 = new_address1
-            self.address2 = new_address2
-            self.city = new_city
-            self.state = new_state
-            self.zip = new_zip
-        elif address_correct == "n":
-            self.reAddress()
-        else:
-            "Invalid Response"
-
-    #Ability to add/edit e-mail address
-    def reEmail(self):
-        new_email = str(raw_input("What is the contact's e-mail? "))
-        print ""
-        print "Email: " + new_email
-        print ""
-        email_correct = str(raw_input("Does this look correct? [y/n]"))
-        if email_correct == "y":
-            self.email = new_email
-        elif email_correct == "n":
-            self.reEmail()
-        else:
-            "Invalid Response"
-
+    #Print full information in an easy to read manor
     def fullInfo(self):
-        print "Name:    " + self.sayFullName()
-        print "Phone:   " + self.pretty_phone
-        print "Email:   " + self.email
+        print
+        print " Name:    " + self.sayFullName()
+        print " Phone:   " + self.pretty_phone
+        print " Email:   " + self.email
         if len(self.address1) > 0:
-            print "Address: " + self.address1
+            print " Address: " + self.address1
             if len(self.address2) > 0:
-                print "         " + self.address2
-            print "         " +self.city + ", " + self.state + " " + self.zip
+                print "          " + self.address2
+            print "          " +self.city + ", " + self.state + " " + self.zip
+            print
         else:
-            print "Address: "
+            print " Address: "
+            print
+
+    #Below here is where the reFunctions begin, this is for updating user information easily
+    def reId(self, id):
+        self.contact_id=id
+
+    def reName(self, fname, lname):
+        self.contact_fname=fname.capitalize()
+        self.contact_lname=lname.capitalize()
+
+    def rePhone(self, phone):
+        self.phone=phone
+        self.makePhonePretty()
+
+    def reEmail(self, email):
+        self.email=email
+
+    def reAddress(self, newaddress):
+        self.address1=newaddress[0]
+        self.address2=newaddress[1]
+        self.city=newaddress[2].capitalize()
+        if len(newaddress[3]) == 2:
+            self.state=newaddress[3].upper()
+        else:
+            self.state=newaddress[3].capitalize()
+        self.zip=newaddress[4]
+
+
 
 #=END Contact Class===
 
 #=END Classes===
-
-#=START Primary Functions===
-def perform_sorting():
-    contact_id_count = 1
-    global contacts_array
-    if len(contacts_array) > 0:
-        contacts_array.sort(key=operator.attrgetter('contact_fname'))
-        for row in contacts_array:
-            row.reId(contact_id_count)
-            contact_id_count += 1
-
-def write_contacts(filename):
-    perform_sorting()
-    output_contacts_file = csv.writer(open(filename, 'w'))
-    for row in contacts_array:
-        output_contacts_file.writerow(row.writeMe())
-
-def fetch_contacts(filename):
-    contacts_contacts_file=csv.reader(open(filename))
-    for row in contacts_contacts_file:
-        contacts_array.append ( Contact(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9]) ) #Split row into class attributes
-    perform_sorting()
-
-def print_contacts():
-    for row in contacts_array:
-        row.listMe()
-
-
-def add_contact():
-    add_loop = True
-    while add_loop:
-        print "Add a Contact: "
-        fname=str(raw_input("What is the contact's first name?: "))
-        lname=str(raw_input("What is the contact's last name?:  "))
-        number=str(raw_input("What is the contact's number?:     "))
-        email=str(raw_input("What is the contact's e-mail?:     "))
-        print "Does this look right?"
-        fullname = fname.capitalize() + " " + lname.capitalize()
-        print "Name:   " + fullname
-        pretty_number = ""
-        if len(number) == 7:
-            pretty_number = number[0:3] + "-" + number[3:]
-        elif len(number) == 10:
-            pretty_number = "(" + number[0:3] + ") " + number[3:6] + "-"+ number[6:]
-        print "Phone:  " + pretty_number
-        print "E-mail: " + email
-        correct=str(raw_input("[y/n]"))
-        if correct == "y":
-            fname = fname.capitalize()
-            lname = lname.capitalize()
-            contacts_array.append(Contact("",fname,lname,number,email))
-        elif correct == "n":
-            pass
-        else:
-            print "Not an option!"
-        another=str(raw_input("Add another? [y/n]: "))
-        if another == "y":
-            pass
-        elif another == "n":
-            add_loop = False
-        else:
-            print "Not an option!"
-    perform_sorting()
-
-
-def remove_contact():
-    remove_loop = True
-    while remove_loop:
-        if len(contacts_array) > 0:
-            perform_sorting()
-            print_contacts()
-            toremove=int(raw_input("Enter ID to remove: "))
-            toremove -= 1
-            if toremove < len(contacts_array):
-                print "Are you sure you want to remove " + contacts_array[toremove].sayName()
-                last_chance=str(raw_input("[y/n]"))
-                if (last_chance.lower() == "y"):
-                    removed_name=contacts_array[toremove].sayName()
-                    del contacts_array[toremove]
-                    print "Removed " + removed_name
-                else:
-                    print "Did NOT delete "+contacts_array[toremove].sayName()
-            else:
-                print "Invalid contact id"
-            another=str(raw_input("Remove another? [y/n]"))
-            if another.lower() == "y":
-                pass
-            else:
-                remove_loop = False
-        else:
-            print "No Contacts!"
-            remove_loop = False
-    perform_sorting()
-
-def append_contact():
-    append_loop = True
-    while append_loop:
-        print ""
-        print_contacts()
-        append_to=int(raw_input("Which contact would you like to add to? "))
-        append_to -= 1
-        if append_to < len(contacts_array):
-            print ""
-            print "Append/Edit Options"
-            print "1. First and Last Name"
-            print "2. Phone"
-            print "3. Email"
-            print "4. Address"
-            print "5. Wrong Contact"
-            append_selection=str(raw_input("What would you like to edit/append on " + contacts_array[append_to].sayFullName() + "?"))
-            if append_selection == "1":
-                contacts_array[append_to].reName()
-            elif append_selection == "2":
-                contacts_array[append_to].rePhone()
-            elif append_selection == "3":
-                contacts_array[append_to].reEmail()
-            elif append_selection == "4":
-                contacts_array[append_to].reAddress()
-            else:
-                pass
-        else:
-            print "Not a valid contact"
-        append_another = str(raw_input("Edit More? [y/n]"))
-        if append_another == "y":
-            append_loop = True
-        elif append_another == "n":
-            append_loop = False
-        else:
-            append_loop = False
-    perform_sorting()
-
-def showContactInfo():
-    showMeLoop = True
-    while showMeLoop:
-        if len(contacts_array) > 0:
-            print_contacts()
-            print ""
-            which_contact = int(raw_input("Which contact? "))
-            which_contact -= 1
-            if which_contact < len(contacts_array):
-                print ""
-                contacts_array[which_contact].fullInfo()
-                print ""
-            else:
-                print "Invalid Contact"
-            exitloop = str(raw_input("Display Another? [y/n]"))
-            if exitloop == "y":
-                pass
-            elif exitloop == "n":
-                showMeLoop = False
-            else:
-                "Not a valid option"
-                
-
-        
-#=END Primary Functions===
-
-#=START Menu and Selector Functions===
-def menu():
-    print ""
-    print "Menu:"
-    print "1. List Contacts"
-    print "2. Add a Contact"
-    print "3. Remove a Contact"
-    print "4. Edit a Contact"
-    print "5. List a Contact's Full Info"
-    print "q. Save and Quit"
-    print ""
-    call_correct_function(raw_input("What to do?: "))
-
-def call_correct_function(selection):
-    if str(selection) == "q":
-        global running
-        running = False
-    elif str(selection) == "1":
-        print_contacts()
-    elif str(selection) == "2":
-        add_contact()
-    elif str(selection) == "3":
-        remove_contact()
-    elif str(selection) == "4":
-        append_contact()
-    elif str(selection) == "5":
-        showContactInfo()
-    else:
-        print "Error, not a valid item"
-
-#=END Menu and Selector Functions===
-
-#=START Initial file check and menu loop===
-if os.path.exists(contacts_file):
-    fetch_contacts(contacts_file)
-
-while running:
-    menu()
-
-#=END Menu Loop, write to contacts file===
-
-write_contacts(contacts_file)
 
 #=END===
---
+++ pyContacts_cmd.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python
+##
+# pyContacts v0.4.0 - Copyright Shawn "prjktdtnt" Thompson 2008
+# Simple contact manager for my own use
+#
+"""
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+#
+##
+
+import csv,os,operator,cmd,sys
+from pyContacts import Contact
+
+class pyCCMD(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.intro = """
+    pyContacts is free software, licensed under GPLv3, see http://www.gnu.org/licenses/ for further information.
+    Type help to see commands and their uses
+        """
+        self.prompt = "pyContacts> "
+        self.storage_file = os.path.expanduser('~') + "/.pyContactsCSV"
+        self.contacts_array = []
+
+    def perform_sorting(self):
+        if len(self.contacts_array) > 0:
+            contact_id_count = 1
+            self.contacts_array.sort(key=operator.attrgetter('contact_fname'))
+            for row in self.contacts_array:
+                row.reId(contact_id_count)
+                contact_id_count += 1
+
+    def load_contacts(self):
+        load_from = csv.reader(open(self.storage_file))
+        for row in load_from:
+            self.contacts_array.append( Contact(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9]) )
+        self.perform_sorting()
+
+    def do_save(self):
+        """Write contacts to main file"""
+
+        self.perform_sorting()
+        save_to = csv.writer(open(self.storage_file, 'w'))
+        for row in self.contacts_array:
+            save_to.writerow(row.writeMe())
+        print "Saved"
+        print
+
+    def do_quit(self, line):
+        """Save contacts and Quit."""
+
+        print
+        self.do_save()
+        sys.exit()
+
+    def do_list(self, noargs):
+        """List contacts"""
+
+        self.perform_sorting()
+        for row in self.contacts_array:
+            row.listMe()
+    
+    def do_show(self, cid):
+        """Show a contact's information"""
+        if cid == '':
+            self.do_list("")
+            cid = int(raw_input("Enter a contact ID: "))
+            cid -= 1
+        else:
+            cid = int(cid)
+            cid -= 1
+        if cid < len(self.contacts_array):
+            self.contacts_array[cid].fullInfo()
+            print
+        else:
+            print "Invalid contact ID"
+            print
+
+    def do_remove(self, cid):
+        """Remove a contact"""
+        
+        if cid == '':
+            self.do_list("")
+            cid = int(raw_input("Enter a contact ID: "))
+            cid -= 1
+        else:
+            cid = int(cid)
+            cid -= 1
+        if cid < len(self.contacts_array):
+            self.contacts_array[cid].fullInfo()
+            last_chance = str(raw_input("Are you sure you want to remove this contact? [y/n]: "))
+            if last_chance.lower() == 'y':
+                removed_name = self.contacts_array[cid].sayName()
+                del self.contacts_array[cid]
+                print "Removed " + removed_name
+                print
+            else:
+                print "Did not delete " + self.contacts_array[cid].sayName()
+                print
+        else:
+            print "Invalid contact ID"
+            print
+
+    def do_add(self, noargs):
+        """Add a new contact"""
+        print "All questions in the following selection except for a name are optional"
+        print "just press enter to skip a question"
+        print
+        fname=str(raw_input("What is the contact's first name?:    "))
+        lname=str(raw_input("What is the contact's last name?:     "))
+        phone=str(raw_input("What is the contact's phone number?:  "))
+        email=str(raw_input("What is the contact's E-mail address? "))
+        print "The following will have you enter the contact's residential address"
+        print "intended for US-Mailing address, this is also optional"
+        print
+        address1=str(raw_input("Address 1: "))
+        address2=str(raw_input("Address 2: "))
+        city=str(raw_input("City: "))
+        state=str(raw_input("State: "))
+        zip=str(raw_input("Zipcode: "))
+        print "The new contact's information is displayed below:"
+        print
+        new_id=len(self.contacts_array)+1
+        self.contacts_array.append(Contact(str(new_id),fname,lname,phone,email,address1,address2,city,state,zip))
+        self.contacts_array[new_id-1].fullInfo()
+        print
+        correct=str(raw_input("Press enter to continue"))
+        self.perform_sorting()
+
+    def do_edit(self, what=""):
+        """ Update a contact's information
+   Example: edit address
+        """
+
+        #If what is not defined then define it
+        if what == '':
+            print "What would you like to edit?"
+            print "1. First and/or Last name"
+            print "2. Phone number"
+            print "3. E-mail Address"
+            print "4. Street Address"
+            what_sel=str(raw_input("Selection: "))
+            if what_sel=='1':
+                what="name"
+            elif what_sel=='2':
+                what="phone"
+            elif what_sel=='3':
+                what="email"
+            elif what_sel=='4':
+                what="address"
+            else:
+                print
+                print "That wasn't an option"
+
+        #Once what is defined, make sure it is valid
+        if what == 'name':
+            pass
+        elif what == 'phone':
+            pass
+        elif what == 'email':
+            pass
+        elif what == 'address':
+            pass
+        else:
+            print
+            print "Invalid edit selection"
+            what = "invalid"
+            cid = len(self.contacts_array)+2
+
+        #Check to see if there is a contact ID already selected,
+        #if not ask user to select one
+        self.do_list("")
+        cid = str(raw_input("Which contact do you want to edit?  "))
+
+        #Make sure cid actually exists
+        if (int(cid)-1) > len(self.contacts_array):
+            print
+            print "Invalid Contact"
+            cid = "invalid"
+
+        #Check to make sure there was not an error above, if there was pass back to main menu
+        if cid != "invalid" and what != "invalid":
+            cid = int(cid)-1
+            self.perform_edit(cid,what)
+        else:
+            print
+            print "There was an error, please try again"
+            print
+
+    #if no error during do_edit then perform the edit
+    def perform_edit(self, cid, what):
+        if what == 'name':
+            old_fname=self.contacts_array[cid].sayfName()
+            old_lname=self.contacts_array[cid].saylName()
+            old_fullName=self.contacts_array[cid].sayFullName()
+            print "You are about to update " + old_fullName
+            print "to make no change just press enter at the prompt"
+            print
+            new_fname=str(raw_input("First Name: "))
+            new_lname=str(raw_input("Last Name:  "))
+            if new_fname == '':
+                new_fname = old_fname
+            if new_lname == '':
+                new_lname = old_lname
+            self.contacts_array[cid].reName(new_fname,new_lname)
+
+        elif what == 'phone':
+            old_phone=self.contacts_array[cid].sayPhone()
+            print "You are about to update the phone number for " + self.contacts_array[cid].sayFullName()
+            print "to cancel just press enter"
+            print
+            new_phone=str(raw_input("New Phone: "))
+            if new_phone == '':
+                new_phone=old_phone
+            self.contacts_array[cid].rePhone(new_phone)
+
+        elif what == 'email':
+            old_email=self.contacts_array[cid].sayEmail()
+            print "Updating e-mail address for " + self.contacts_array[cid].sayFullName()
+            print "to cancel just press enter"
+            print
+            new_email=str(raw_input("New Email: "))
+            if new_email == '':
+                new_email = old_email
+            self.contacts_array[cid].reEmail(new_email)
+
+        elif what == 'address':
+            old_address=self.contacts_array[cid].sayAddress()
+            print "Updating e-mail address for " + self.contacts_array[cid].sayFullName()
+            print "to cancel this leave all fields blank"
+            print
+            address1=str(raw_input("Address 1: "))
+            address2=str(raw_input("Address 2: "))
+            city=str(raw_input("City: "))
+            state=str(raw_input("State: "))
+            zip=str(raw_input("Zip: "))
+            new=[address1,address2,city,state,zip]
+            if len(new[0]) == 0 and len(new[1]) == 0 and len(new[2]) == 0 and len(new[3]) == 0 and len(new[4]) == 0:
+                new=old_address
+            self.contacts_array[cid].reAddress(new)
+
+        else:
+            print "Something has gone terribly wrong!!!" #If you get here, there's a SERIOUS issue somewhere
+
+
+    def do_version(self, noargs):
+        """Show program version"""
+        print
+        print "  pyContacts v0.4.0"
+        print
+    
+#Internal start
+def main():
+    pyCCMD_cmd = pyCCMD()
+    pyCCMD_cmd.load_contacts()
+    pyCCMD_cmd.cmdloop()
+
+#External start
+if __name__ == "__main__":
+    main()
