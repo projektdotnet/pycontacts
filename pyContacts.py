f0311c67ff3805c10b0b4eab40dff6aa19de2175
Shawn Thompson <shawn@projektenterprises.com>
2010-09-14 09:53:49 -0600

    Remove obvious comments

--- pyContacts.py
+++ pyContacts.py
@@ -20,10 +20,8 @@
 class Contact:
     def __init__(self, id="", fname="", lname="", phone="", email="", address1="", address2="", city="", state="", zip=""):
         self.contact_id = id
-        #Capitalize fname and lname (for newly added contacts)
         self.contact_fname = fname.capitalize()
         self.contact_lname = lname.capitalize()
-        #Receive the phone info
         self.phone = phone
         self.makePhonePretty()
         self.email = email
@@ -36,28 +34,22 @@ class Contact:
             self.state = state.capitalize()
         self.zip = zip
 
-    #listMe used to print a clean looking version with proper spacing
     def listMe(self):
         return [str(self.contact_id),self.sayFullName()]
 
-    #List output for other functions to use
     def writeMe(self):
         return [self.contact_id,self.contact_fname,self.contact_lname,self.phone,self.email,self.address1,self.address2,self.city,self.state,self.zip]
 
-    #Return only first name
     def sayfName(self):
         return self.contact_fname
 
-    #Return only last name 
     def saylName(self):
         return self.contact_lname
     
-    #Return the full name of a user
     def sayFullName(self):
         myFullName = self.contact_fname + " " + self.contact_lname
         return myFullName
 
-    #Return the User ID
     def sayId(self):
         return self.contact_id
     
@@ -70,8 +62,6 @@ class Contact:
     def sayAddress(self):
         return [self.address1,self.address2,self.city,self.state,self.zip]
         
-
-    #Create a user readable phone
     def makePhonePretty(self):
         if len(self.phone) == 7:
             self.pretty_phone = self.phone[0:3] + '-' + self.phone[3:] #ex 555-1234
@@ -80,11 +70,9 @@ class Contact:
         else:
             self.pretty_phone = self.phone # If we don't know the correct output then don't attempt to format ex 815551234
 
-    #Print full information in an easy to read manor
     def fullInfo(self):
         return [self.sayFullName(),self.pretty_phone,self.email,self.address1,self.address2,self.city,self.state,self.zip]
 
-    #Below here is where the reFunctions begin, this is for updating user information easily
     def reId(self, id):
         self.contact_id=id
 
@@ -108,8 +96,6 @@ class Contact:
         else:
             self.state=newaddress[3].capitalize()
         self.zip=newaddress[4]
-
-
 
 #=END Contact Class===
 
